57b48675ff1eb5312d121723d1d51d22b7547c0d
Will Grieger <165510301+wgrieger@users.noreply.github.com>
2024-04-23 03:11:32 +0000

    done

--- app.rb
+++ app.rb
@@ -56,44 +56,41 @@ require "json"
 
 
  get ("/:countrycode1") do 
+  #list_access
+      exchange_key= ENV["EXCHANGE"]
+      base= "https://api.exchangerate.host/"
+      list_resource= "list"
+      key= "?access_key=" + exchange_key.to_s
+      @list_url= base + list_resource + key
+      list_data_raw= HTTP.get(@list_url)
+      json_list_data=JSON.parse(list_data_raw)
 
-#list access
-    exchange_key= ENV["EXCHANGE"]
-    base= "https://api.exchangerate.host/"
-    list_resource= "list"
-    key= "?access_key=" + exchange_key.to_s
-    @list_url= base + list_resource + key
-    list_data_raw= HTTP.get(@list_url)
-    json_list_data=JSON.parse(list_data_raw)
+      @all_currencies_hash= json_list_data.fetch("currencies")
+      @country_codes=(@all_currencies_hash.keys)
 
-    @all_currencies_hash= json_list_data.fetch("currencies")
-    @country_codes=(@all_currencies_hash.keys)
-
-
-@country1=params.fetch("countrycode1").to_s
-
-
-erb (:display)
-end
-
-get ("/:countrycode1/:countrycode2") do
 
   @country1=params.fetch("countrycode1").to_s
-  @country2=params.fetch("countrycode2").to_s
+  erb (:display)
+  end
 
-#converting
-  exchange_key= ENV["EXCHANGE"]
-  base= "https://api.exchangerate.host/"
-  convert_resource= "convert"
-  key= "?access_key=" + exchange_key.to_s
-  exchange_ticks= "&from=" + @country1 + "&to="+ @country2 + "&amount=1"
-  @data_url= base + convert_resource + key + exchange_ticks
+  get ("/:countrycode1/:countrycode2") do
 
-  raw_data= HTTP.get(@data_url)
-  json_convert_data=JSON.parse(raw_data)
+    @country1=params.fetch("countrycode1").to_s
+    @country2=params.fetch("countrycode2").to_s
 
-  @result= json_convert_data.fetch("result").to_s
-  #converting end
+  #converting
+    exchange_key= ENV["EXCHANGE"]
+    base= "https://api.exchangerate.host/"
+    convert_resource= "convert"
+    key= "?access_key=" + exchange_key.to_s
+    exchange_ticks= "&from=" + @country1 + "&to="+ @country2 + "&amount=1"
+    @data_url= base + convert_resource + key + exchange_ticks
+
+    raw_data= HTTP.get(@data_url)
+    json_convert_data=JSON.parse(raw_data)
+
+    @result= json_convert_data.fetch("result").to_s
+    #converting end
 
   erb (:result)
-end
+  end
